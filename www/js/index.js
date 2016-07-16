b4dc8bde85060ca323370a0f8b8be76bdc427ce3
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-07-16 16:00:32 +0300

    ** Added sidebar with icons

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,13 @@ var LOCATION_OPTIONS = {
   timeout: 5000,
   enableHighAccuracy: true 
 };
+var PlayerIcon = L.Icon.Default.extend({
+  options: {
+    iconUrl: 'img/gangster.svg',
+    iconSize: [54, 57],
+    shadowSize: [0, 0]
+  }
+});
 
 var app = {
   initialize: function() {
@@ -17,8 +24,9 @@ var app = {
   },
   onLocationUpdated: function(pos) {
     if (!player) {
+      var playerIcon = new PlayerIcon();
       map.setView(new L.LatLng(pos.coords.latitude, pos.coords.longitude), 18);
-      player = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map);
+      player = L.marker([pos.coords.latitude, pos.coords.longitude], { icon: playerIcon }).addTo(map);
     } else {
       player.setLatLng([pos.coords.latitude, pos.coords.longitude]);
     }
@@ -27,13 +35,14 @@ var app = {
     alert(JSON.stringify(err));
   },
   onDeviceReady: function() {
-    map = new L.Map('map');
+    map = new L.Map('map', { zoomControl:false });
   	var tiles = new L.TileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
       minZoom: 16,
       maxZoom: 18,
       attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
     });
   	map.addLayer(tiles);
+    L.control.sidebar('sidebar').addTo(map);
     navigator.geolocation.watchPosition(
       app.onLocationUpdated,
       app.onLocationError,
